b9269fd7942a5c6d49d52edcda53d5c907f3c762
heriming13 <heriming13@126.com>
2025-09-06 11:45:20 +0800

    Update run_all.R

--- analysis/run_all.R
+++ analysis/run_all.R
@@ -1,1 +1,6 @@
+source("analysis/01_data_processing.R")
+source("analysis/02_trajectory_modeling.R")
+source("analysis/03_survival_Cox.R")
+source("analysis/04_sensitivity_transfusion.R")
+source("analysis/05_tables_figures.R")
 
